f834972aaad7b3f6afb2be09d91a0ffdeabdfb8b
n4l5u0r <59141606+n4l5u0r@users.noreply.github.com>
2024-11-30 01:38:49 +0100

    fix: fuzzing

--- src/handle_init_contract.c
+++ src/handle_init_contract.c
@@ -25,7 +25,8 @@ void handle_init_contract(ethPluginInitContract_t *parameters) {
     context->valid = 1;
 
     for (uint8_t i = 0; i < NUM_PARASWAP_SELECTORS; i++) {
-        if (memcmp((uint8_t *) PIC(PARASWAP_SELECTORS[i]), msg->selector, SELECTOR_SIZE) == 0) {
+        if (memcmp((uint8_t *) pic((void *) PARASWAP_SELECTORS[i]), msg->selector, SELECTOR_SIZE) ==
+            0) {
             context->selectorIndex = i;
             break;
         }
