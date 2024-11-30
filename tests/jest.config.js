e032fb2235f091fb00260b09da429eb3780dad10
n4l5u0r <59141606+n4l5u0r@users.noreply.github.com>
2024-11-30 13:48:33 +0100

    test: adjustments

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -4,6 +4,5 @@ module.exports = {
   preset: 'ts-jest',
   bail: false,
   maxWorkers: 1,
-  transformIgnorePatterns: ['^.+\\.js$'],
-  //testMatch: ['**/eth/v4/simple_swap.v4.test.ts','**/eth/v5/swap_on_zero_v4.test.ts','**/eth/v5/multi_swap.test.ts'],
+  transformIgnorePatterns: ['^.+\\.js$']
 };
